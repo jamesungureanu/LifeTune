1dc8e8f51262575c6f8def8e07532c9e5afa9316
jamesungureanu <ungureanu792733@sas.edu.sg>
2025-12-31 17:28:19 +0800

    Update static.ts

--- server/static.ts
+++ server/static.ts
@@ -3,7 +3,9 @@ import fs from "fs";
 import path from "path";
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(__dirname, "public");
+  // dist/server → dist/public
+  const distPath = path.resolve(__dirname, "..", "public");
+
   if (!fs.existsSync(distPath)) {
     throw new Error(
       `Could not find the build directory: ${distPath}, make sure to build the client first`,
@@ -12,8 +14,8 @@ export function serveStatic(app: Express) {
 
   app.use(express.static(distPath));
 
-  // fall through to index.html if the file doesn't exist
+  // SPA fallback (for routes like /game)
   app.use("*", (_req, res) => {
-    res.sendFile(path.resolve(distPath, "index.html"));
+    res.sendFile(path.join(distPath, "index.html"));
   });
 }
